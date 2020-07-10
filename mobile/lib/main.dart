065b9fba8bfbd68e1c979b68e2b49d2e68688c80
roman <greenfield0000@gmail.com>
2020-07-11 00:22:12 +0300

    add main-page

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:mobile/pages/login-page.dart';
+
+import 'pages/main-page.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,6 +18,9 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
+      routes: {
+        '/main' : (BuildContext context) => MainPage(tittle: 'Flutter Demo Home Page')
+      } ,
     );
   }
 }
@@ -31,80 +37,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.pink, Colors.blue]),
-        ),
-        child: Container(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          margin: EdgeInsets.symmetric(horizontal: 50),
-          alignment: Alignment.center,
-          child: Center(
-            child: Card(
-              margin: EdgeInsets.symmetric(vertical: 85.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(25)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'SIGN IN',
-                    style: TextStyle(fontSize: 30.0),
-                  ),
-                  const SizedBox(height: 30),
-                  TextField(
-                    decoration: InputDecoration(
-                      fillColor: Colors.pink,
-                      border: new OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(25.0),
-                        ),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.email,
-                        color: Colors.black,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      fillColor: Colors.pink,
-                      border: OutlineInputBorder(
-                        borderRadius: const BorderRadius.all(
-                          const Radius.circular(25.0),
-                        ),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.lock,
-                        color: Colors.black,
-                        size: 30.0,
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 30),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text(
-                      'Login'.toUpperCase(),
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    color: Colors.pink,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(25)),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
+    return LoginPage();
   }
 }
---
+++ mobile/lib/pages/login-page.dart
@@ -0,0 +1,82 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          margin: EdgeInsets.symmetric(horizontal: 50),
+          alignment: Alignment.center,
+          child: Center(
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 85.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'SIGN IN',
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                  const SizedBox(height: 30),
+                  TextField(
+                    decoration: InputDecoration(
+                      fillColor: Colors.pink,
+                      border: new OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(25.0),
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Colors.black,
+                        size: 30.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  TextField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      fillColor: Colors.pink,
+                      border: OutlineInputBorder(
+                        borderRadius: const BorderRadius.all(
+                          const Radius.circular(25.0),
+                        ),
+                      ),
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Colors.black,
+                        size: 30.0,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 30),
+                  RaisedButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/main');
+                    },
+                    child: Text(
+                      'Login'.toUpperCase(),
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    color: Colors.pink,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(25)),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+    );
+  }
+}
